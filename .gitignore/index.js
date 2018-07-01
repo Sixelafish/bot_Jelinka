b0b180aceae44a04680c12d62ef6139ffe2018d1
Sixelafish <37385880+Sixelafish@users.noreply.github.com>
2018-07-01 22:14:05 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix =  ("*")
 
 bot.on('ready', function() {
-	bot.user.setGame("Version 0.2.0");
+	bot.user.setGame("Version 0.2.1");
 	console.log("Connected");
 });
 
@@ -15,7 +15,7 @@ bot.on('message', message => {
 		message.channel.sendMessage("```\n Liste des commandes : \n \n *help - Affiche les commandes \n ```");
 	}
 
-	if (message.content === prefix + "Bourse " + "Créer"){
+	if (message.content === prefix + "BourseCréer"){
 		message.channel.sendMessage("Votre bourse à été créer " + member.displayName + " \n \n \n *C'est un test, en vrai vous avez pas de bourse*")
 
 	}
