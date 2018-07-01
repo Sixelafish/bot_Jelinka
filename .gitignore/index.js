b945fe60e0d86b767277436a526f7c04d1c31f6d
Sixelafish <37385880+Sixelafish@users.noreply.github.com>
2018-07-01 22:08:42 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,20 +4,20 @@ const bot = new Discord.Client();
 var prefix =  ("*")
 
 bot.on('ready', function() {
-	bot.user.setGame("Version 0.2.1");
+	bot.user.setGame("Version 0.2.0");
 	console.log("Connected");
 });
 
 bot.login(process.env.TOKEN);
 
-bot.on('guildMemberAdd', member => {
-  create var money.DisplayName = 0
-    return channel.send('Voici ton argent : ' + money.displayName + member.displayName)
-  }).catch(console.error)
-
 bot.on('message', message => {
 	if (message.content === prefix + "help"){
 		message.channel.sendMessage("```\n Liste des commandes : \n \n *help - Affiche les commandes \n ```");
+	}
+
+	if(message.content === prefix + "Bourse " + "Créer"){
+		message.channel.sendMessage("Votre bourse à été créer " + member.displayName + " \n \n \n *C'est un test, en vrai vous avez pas de bourse*")
+
 	}
 
 
