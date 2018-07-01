3b6a16cdb5748fc34adec581d68059ecb498ca9e
Sixelafish <37385880+Sixelafish@users.noreply.github.com>
2018-07-01 20:43:24 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,8 +4,13 @@ const bot = new Discord.Client();
 var prefix =  ("*")
 
 bot.on('ready', function() {
-	bot.user.setGame("Version 0.0.1");
+	bot.user.setGame("Commande aide : *help");
 	console.log("Connected");
 });
 
 bot.login(process.env.TOKEN);
+
+bot.on('message', message => {
+	if (message.content === prefix + "help"){
+		message.channel.sendMessage("```\n Liste des commandes : \n *help - Affiche les commandes ");
+	}
