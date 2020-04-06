58d4818e6ee81d48e4ce257ccb3c05354b1493cc
Sixelafish <37385880+Sixelafish@users.noreply.github.com>
2020-04-06 17:21:10 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,18 +9,4 @@ bot.on('ready', function() {
 });
 
 
-bot.on('message', message => {
-	if (message.content === prefix + "help"){
-		message.channel.sendMessage("```\n Liste des commandes : \n \n *help - Affiche les commandes \n ```");
-	}
-
-	if (message.content === prefix + "Bourse Créer"){
-		message.channel.sendMessage("Votre bourse à été créer "  + <@USER_ID> + " \n \n \n *C'est un test, en vrai vous avez pas de bourse*")
-
-	}
-
-
-	}
-);
-
 bot.login(process.env.TOKEN);
