00550ce3f4b9624c813d5eb366e1c37abdbf4d18
Sixelafish <37385880+Sixelafish@users.noreply.github.com>
2020-04-13 01:15:40 +0200

    Add files via upload

---
+++ .gitignore/bot.js
@@ -0,0 +1,19 @@
+
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+
+bot.login(process.env.TOKEN);
+
+;
